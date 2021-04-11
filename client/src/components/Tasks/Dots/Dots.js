042a86618165680013dff66b36e1274578600861
dawum nam <42849054+dawumnam@users.noreply.github.com>
2021-04-10 16:09:55 -0800

    Added dummy to display dots

--- client/src/components/Tasks/Dots/Dots.js
+++ client/src/components/Tasks/Dots/Dots.js
@@ -14,6 +14,26 @@ function Dots(props) {
           "#30a14e";
       });
 
+    const dummy = () => {
+      let dummyArray = [];
+      for (let i = 0; i < 30; i++) {
+        let date = new Date();
+        date.setFullYear(
+          2021,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 29)
+        );
+        date.setHours(0, 0, 0, 0);
+        const newDate = date.toISOString().split("T")[0];
+        dummyArray.push(newDate);
+        dummyArray.forEach((e) => {
+          document.getElementById(`${e}-${taskId}`).style.backgroundColor =
+            "#30a14e";
+        });
+      }
+    };
+    dummy();
+
     return () => {};
   }, []);
   return (
